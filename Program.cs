7a789e6f1d93d30c6cf055528835e06d90ead123
malak1070 <malaktamer159@outlook.com>
2026-02-09 23:00:24 +0200

    Add files via upload
    task4

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+class InsufficientBalanceException : Exception
+{
+    public InsufficientBalanceException(string message) : base(message)
+    {
+    }
+}
+
+class PaymentTimeoutException : Exception
+{
+    public PaymentTimeoutException(string message) : base(message)
+    {
+    }
+}
+
+class PaymentSystem
+{
+    public void Pay(double balance, double amount)
+    {
+    if (amount > balance)
+        {
+            throw new InsufficientBalanceException("Insufficient balance for this payment.");
+        }
+
+        if (amount == 500)
+        {
+            throw new PaymentTimeoutException("Payment process timed out.");
+        }
+
+        Console.WriteLine("Payment completed successfully.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+     PaymentSystem payment = new PaymentSystem();
+
+    try
+    {
+      payment.Pay(300, 500);
+        }
+    catch (InsufficientBalanceException ex)
+        {
+            Console.WriteLine("Balance Error: " + ex.Message);
+        }
+    catch (PaymentTimeoutException ex)
+        {
+            Console.WriteLine("Timeout Error: " + ex.Message);
+        }
+       
+    catch (Exception ex)
+        {
+            Console.WriteLine("General Error: " + ex.Message);
+        }
+    finally
+        {
+            Console.WriteLine("Payment attempt finished.");
+        }
+    }
+}
+
